a6c50a7079aeb3dd6cc8125730d6df932d6087b4
Santiago Dimaren <sdimaren1@gmail.com>
2017-07-19 11:54:00 -0400

    added module 13

---
+++ 13 - JavaScript Modules and Using npm/es6modules/src/config.js
@@ -0,0 +1,13 @@
+// named export
+export const apiKey = 'abc123';
+
+export const url = 'http://wesbos.com';
+
+export function sayHi(name) {
+	console.log(`Hello there ${name}`);
+}
+
+const age = 100;
+const dog = 'Snickers';
+
+export { age as old, dog };
