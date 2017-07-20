af0e2259602c3f61dfacacf7d337d18d34098036
Santiago Dimaren <sdimaren1@gmail.com>
2017-07-20 12:25:23 -0400

    added module 14

---
+++ 14 - ES6 Tooling/babel/app.js
@@ -0,0 +1,10 @@
+let { x, y, ...z } = { x: 1, y: 2, a: 3, b: 4 };
+
+const age = 100;
+const people = ['Wes', 'Kait'];
+
+const fullNames = people.map(name => `${name} Bos`);
+
+for (person of people) {
+	console.log(person);
+}
